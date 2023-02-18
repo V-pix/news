fe197dec5d47f7ca9cb855cf6080fce7c354ee22
veronika <veronika@v-pix.local>
2023-02-18 11:01:52 +0530

    README

--- news/news/settings.py
+++ news/news/settings.py
@@ -1,14 +1,15 @@
 import os
 from datetime import timedelta
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv("SECRET_KEY", default="secret")
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
+# ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["*"]
 
 
 INSTALLED_APPS = [
